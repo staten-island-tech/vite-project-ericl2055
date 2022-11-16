297c61a48d085d07daff8953ecec95e43bce519a
ericl2055 <eric_lin2055@icloud.com>
2022-11-16 14:01:15 -0500

    bon

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,4 @@
+import "./style.css";
 document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList = "dark";
